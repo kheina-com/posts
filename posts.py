94abdcf0a6ed93cb806d6705d939505ab6f2b85d
kheina <vixen@kheina.com>
2022-03-05 15:58:30 -0500

    set sort method from tag

--- posts.py
+++ posts.py
@@ -193,6 +193,13 @@ class Posts(UserBlocking) :
 					(exclude_rating if exclude else include_rating).append(tag)
 					continue
 
+				if tag.startswith('sort:') :
+					try :
+						sort = PostSort[tag[5:]]
+
+					except KeyError :
+						raise BadRequest(f'{tag[5:]} is not a valid sort method. valid methods: {list(PostSort.__members__.keys())}')
+
 				(exclude_tags if exclude else include_tags).append(tag)
 
 			if len(include_users) > 1 :
