c35b539feea3b7e3fce0b2ed0ef9fd200e02dd25
kheina <vixen@kheina.com>
2023-01-03 21:37:59 -0500

    swap drafts to get

--- server.py
+++ server.py
@@ -73,7 +73,7 @@ async def v1FetchMyPosts(req: Request, body: BaseFetchRequest) -> List[Post] :
 	return await posts.fetchOwnPosts(req.user, body.sort, body.count, body.page)
 
 
-@app.post('/v1/fetch_drafts', responses={ 200: { 'model': List[Post] } })
+@app.get('/v1/fetch_drafts', responses={ 200: { 'model': List[Post] } })
 async def v1FetchDrafts(req: Request) -> List[Post] :
 	await req.user.authenticated()
 	return await posts.fetchDrafts(req.user)
