ae77c244f9a72b5f383465789551298eb2cb695a
kheina <vixen@kheina.com>
2023-01-05 23:15:17 -0500

    bump version

--- fuzzly_posts/__init__.py
+++ fuzzly_posts/__init__.py
@@ -6,7 +6,7 @@ from kh_common.gateway import Gateway
 from fuzzly_posts.models import Post
 
 
-__version__: str = '0.0.4'
+__version__: str = '0.0.5'
 
 
 PostGateway: Gateway = Gateway(posts_host + '/v1/post/{post}', Post, 'GET')
